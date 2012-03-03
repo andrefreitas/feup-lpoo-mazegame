9b6d8916fd622507027b364b760d3682bdd59e26
Andre Freitas <p.andrefreitas@gmail.com>
2012-03-03 13:02:42 +0000

    added code comments, optimization of moveDragon() and dragon killing bug fixed

--- MazeGame.java
+++ MazeGame.java
@@ -2,16 +2,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.CharBuffer;
-
-/**
- * @version 1.0
- * @created 27-Fev-2012 14:58:57
- */
+/********************************************************
+ * This class defines a maze game
+ ********************************************************/
 public class MazeGame {
-
+	/* Attributes ================== */
 	private static GameObject dragon;
 	private static GameObject hero;
 	private static GameObject exit;
+	private static GameObject sword;
 	private static char[][] mazeMap = {
 			{ 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
 			{ 'X', 'H', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
@@ -23,67 +22,78 @@ public class MazeGame {
 			{ 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' },
 			{ 'X', 'E', 'X', 'X', ' ', ' ', ' ', ' ', ' ', 'X' },
 			{ 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' } };
-	private static GameObject sword;
-
-	public MazeGame() {
-
-	}
-
+	
+	/* main()  ================== */
 	public static void main(String args[]) throws IOException {
 		play();
 	}
-
-	public void finalize() throws Throwable {
-
+	
+	// this function is called by main and is the game cycle itself
+	private static void play() throws IOException {
+		setupObjects();
+		do {
+			printMaze();
+			moveHero(readKeyboardArrow());
+			moveDragon();
+		} while (!gameOver());
+		printMaze();
+	}
+	
+	// setup the object with their initial postion and states
+	private static void setupObjects() {
+		hero = new GameObject('H', 1, 1);
+		exit = new GameObject('S', 9, 5);
+		sword = new GameObject('E', 1, 8);
+		dragon = new GameObject('D', 1, 3);
+	}
+	
+	// print the maze in the standard output
+	private static void printMaze() {
+		for (int i = 0; i < 10; i++) {
+			for (int n = 0; n < 10; n++) {
+				System.out.print(mazeMap[i][n] + " ");
+			}
+			System.out.print('\n');
+		}
 	}
 
+	// this function evaluates if the game is over by checking if the hero is unarmed or is near a dragon
 	private static boolean gameOver() {
 		return ((GameObject.samePosition(hero, exit) && hero.getState() == 'A') || (GameObject
 				.adjacentPosition(hero, dragon) && hero.getState() == 'H'));
 	}
-
+	
+	// random maze generation
 	private void generateMaze() {
 
 	}
-
+	
+	// move the dragon randomly
 	private static void moveDragon() {
+		if (dragon.getState()=='K') return ; // bug fix 1
 		int x = 0, y = 0;
+		int deltaX[]={0,0,-1,1,0};
+		int deltaY[]={-1,1,0,0,0};
 		java.util.Random r = new java.util.Random();
 		do {
 			int i = r.nextInt(5);
-
-			switch (i) {
-			case 0:
-				x = 0;
-				y = -1;
-				break;
-			case 1:
-				x = 0;
-				y = 1;
-				break;
-			case 2:
-				x = -1;
-				y = 0;
-				break;
-			case 3:
-				x = 1;
-				y = 0;
-				break;
-			case 4:
-				x = 0;
-				y = 0;
-				break;
-			}
+			x = deltaX[i];
+			y = deltaY[i];
 		} while (mazeMap[dragon.getY() + y][dragon.getX() + x] == 'X'
 				|| mazeMap[dragon.getY() + y][dragon.getX() + x] == 'S');
+		
+		// the dragon moves if only is not near the hero
 		if (!GameObject.adjacentPosition(dragon, hero)) {
+			// delete the old cell and move the dragon to the new cell
 			mazeMap[dragon.getY()][dragon.getX()] = ' ';
 			mazeMap[dragon.getY() + y][dragon.getX() + x] = dragon.getState();
 			updateObject(dragon, dragon.getX() + x, dragon.getY() + y,
 					dragon.getState());
+			// if the dragon goes to a cell with a sword the status changes to F
 			if (GameObject.samePosition(dragon, sword)) {
 				mazeMap[dragon.getY()][dragon.getX()] = 'F';
 				updateObject(dragon, dragon.getX(), dragon.getY(), 'F');
+			// if the new position of the dragon is without the sword, he returns to state D
 			} else if (GameObject.adjacentPosition(dragon, sword)
 					&& dragon.getState() == 'F') {
 				updateObject(dragon, dragon.getX(), dragon.getY(), 'D');
@@ -93,10 +103,7 @@ public class MazeGame {
 		}
 	}
 
-	/**
-	 * 
-	 * @param char
-	 */
+	// move the hero by receiving an input
 	private static void moveHero(char direction) {
 		int x = 0, y = 0;
 		switch (direction) {
@@ -117,6 +124,7 @@ public class MazeGame {
 			y = 0;
 			break;
 		}
+		// only moves the heroe if the new cell isn't a wall and if he's not unarmed and going to the exit
 		if (mazeMap[hero.getY() + y][hero.getX() + x] != 'X'
 				&& !(hero.getX() + x == exit.getX()
 						&& hero.getY() + y == exit.getY() && hero.getState() == 'H')) {
@@ -126,34 +134,21 @@ public class MazeGame {
 					hero.getState());
 
 		}
+		// if the hero is in the same cell of the sword, goes to the state "Armed"
 		if (GameObject.samePosition(hero, sword)) {
 			updateObject(hero, hero.getX(), hero.getY(), 'A');
 			mazeMap[hero.getY()][hero.getX()] = hero.getState();
 		}
+		// if the hero is near the dragon and is armed, the dragon dies
 		if (GameObject.adjacentPosition(hero, dragon) && hero.getState() == 'A') {
 			mazeMap[dragon.getY()][dragon.getX()] = ' ';
+			// bug fix 1
+			updateObject(dragon, -1, -1, 'K');
 		}
 	}
 
-	private static void play() throws IOException {
-		setupObjects();
-		do {
-			printMaze();
-			moveHero(readKeyboardArrow());
-			moveDragon();
-		} while (!gameOver());
-		printMaze();
-	}
 
-	private static void printMaze() {
-		for (int i = 0; i < 10; i++) {
-			for (int n = 0; n < 10; n++) {
-				System.out.print(mazeMap[i][n] + " ");
-			}
-			System.out.print('\n');
-		}
-	}
-
+	// read keyboard char input
 	private static char readKeyboardArrow() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		char chars[] = new char[1];
@@ -164,20 +159,9 @@ public class MazeGame {
 		return chars[0];
 	}
 
-	private static void setupObjects() {
-		hero = new GameObject('H', 1, 1);
-		exit = new GameObject('S', 9, 5);
-		sword = new GameObject('E', 1, 8);
-		dragon = new GameObject('D', 1, 3);
-	}
+	
 
-	/**
-	 * 
-	 * @param a
-	 * @param x
-	 * @param y
-	 * @param state
-	 */
+	// Update a gameobject calling the set functions
 	private static void updateObject(GameObject a, int x, int y, char state) {
 		a.setY(y);
 		a.setX(x);
