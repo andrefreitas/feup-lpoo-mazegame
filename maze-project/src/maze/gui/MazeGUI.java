681c5605d9b8bd5f906e6264c49227af02568a8e
andrefreitas <p.andrefreitas@gmail.com>
2012-03-26 15:47:44 +0100

    Enabled dragon self move in the GUI and hero move when key pressed.

--- maze-project/src/maze/gui/MazeGUI.java
+++ maze-project/src/maze/gui/MazeGUI.java
@@ -9,7 +9,7 @@ import javax.swing.*;
 import maze.logic.MazeGame;
 
 public class MazeGUI {
-	private JFrame frame;
+	public JFrame frame;
 
 	public void init() {
 		frame = new JFrame("Maze Game");
@@ -17,9 +17,6 @@ public class MazeGUI {
 		frame.setMinimumSize(new Dimension((MazeGame.maze.mazeDim + 1) * 20,
 				(MazeGame.maze.mazeDim + 2) * 20));
 		frame.setContentPane(new GamePanel());
-		// Criar "widgets" e adicionar à área de conteúdo
-		createWidgets();
-		addWidgets(frame.getContentPane());
 		// Redimensionar e mostrar a janela
 		frame.pack();
 		frame.setVisible(true);
@@ -33,37 +30,30 @@ public class MazeGUI {
 
 			@Override
 			public void keyReleased(KeyEvent e) {
-				// TODO Auto-generated method stub
+				MazeGame.maze.hero.move(Character.toUpperCase(e.getKeyChar()));
+				frame.repaint();
+				if (MazeGame.gameOver())
+					gameOver();
 
 			}
 
 			@Override
 			public void keyPressed(KeyEvent e) {
-				switch (Character.toUpperCase(e.getKeyChar())) {
-				case 'W':
-					System.out.println("W");
-				case 'A':
-					System.out.println("A");
-				case 'S':
-					System.out.println("S");
-				case 'D':
-					System.out.println("D");
-				}
 
 			}
+
 		};
 		frame.getContentPane().addKeyListener(keyList);
 		frame.getContentPane().setFocusable(true);
 
 	}
 
-	private void addWidgets(Container contentPane) {
-	}
-
-	private void createWidgets() {
+	private void gameOver() {
+		System.exit(0);
 
 	}
 
+	// Game Panel that show the maze
 	public class GamePanel extends JPanel {
 		public GamePanel() {
 			super();
--- maze-project/src/maze/logic/MazeGame.java
+++ maze-project/src/maze/logic/MazeGame.java
@@ -7,98 +7,137 @@ import java.util.Scanner;
 
 import maze.cli.MazeCLI;
 import maze.gui.*;
+
 /********************************************************
  * This class defines a maze game
  ********************************************************/
 public class MazeGame {
-	
+
 	public static Maze maze = new Maze();
 	static int d; // dragon option
 	static int nDragons; // number of dragons
+	static boolean ui = true;
+	static MazeGUI gui;
+
 	/* main() ================== */
 	public static void main(String args[]) throws IOException {
 		Scanner in = new Scanner(System.in);
 		char opt;
 		int dim = 0;
-		do {  //ask if the user wants a random maze or the default one
-		
+		do { // ask if the user wants a random maze or the default one
+
 			System.out.print("Generate random maze (Y-N): ");
 			opt = in.nextLine().toCharArray()[0];
 			opt = Character.toUpperCase(opt);
 		} while (opt != 'Y' && opt != 'N');
-		if (opt == 'Y') { //ask the maze dimension
+		if (opt == 'Y') { // ask the maze dimension
 			System.out.print("X and Y Dimension: ");
 			dim = in.nextInt();
 			dim = Math.abs(dim);
 			maze.mazeDim = dim;
 			MazeBuilder.generateMaze(maze.mazeDim);
 		}
-		// Evaluate if the player wants a static dragon, a dragon that moves or a dragon that moves and sleeps
+		// Evaluate if the player wants a static dragon, a dragon that moves or
+		// a dragon that moves and sleeps
 		System.out.println("Choose the type of dragon that you want:");
-		System.out.print("1 - Static Dragon\n2 - Dragon that moves\n3 - Dragon that moves and sleep\n");
+		System.out
+				.print("1 - Static Dragon\n2 - Dragon that moves\n3 - Dragon that moves and sleep\n");
 		System.out.print("Option:");
 		d = in.nextInt();
-		
+
 		// How many dragons?
-		do{
+		do {
 			System.out.println("How many Dragons? (>=1 and <=3):");
-			nDragons=in.nextInt();
+			nDragons = in.nextInt();
+		} while (nDragons < 1 || nDragons > 3);
+		
+		// create a new Mazegui instance
+		if (ui) {
+			gui = new MazeGUI();
+			gui.init();
 		}
-		while(nDragons<1 || nDragons>3);
-		new MazeGUI().init();
 		play();
+	}
+
+	// Wait implementation
+	public static void wait(int n) {
+		long t0, t1;
+		t0 = System.currentTimeMillis();
+		do {
+			t1 = System.currentTimeMillis();
+		} while (t1 - t0 < 1000);
 	}
 
 	// this function is called by main and is the game cycle itself
 	public static void play() throws IOException {
 		setupObjects();
-		do {
+		if (!ui) {
+			// CLI interface
+			do {
+				MazeCLI.printMaze();
+				maze.hero.move(MazeCLI.readKeyboardArrow());
+				maze.moveDragons();
+			} while (!gameOver());
 			MazeCLI.printMaze();
-			maze.hero.move(MazeCLI.readKeyboardArrow());
-			maze.moveDragons();
-		} while (!gameOver());
-		MazeCLI.printMaze();
+		} else {
+			// GUI interface
+			do {
+				maze.moveDragons();
+				gui.frame.repaint();
+				wait(2);
+
+			} while (!gameOver());
+		}
 	}
 
 	// setup the objects with their initial positions and states
 	public static void setupObjects() {
-		maze.hero =new heroObject('H', 1, 1);
+		maze.hero = new heroObject('H', 1, 1);
 		maze.mazeMap[maze.hero.getY()][maze.hero.getX()] = maze.hero.getState();
 
 		maze.sword = new GameObject('E', 1, 8);
-		maze.mazeMap[maze.sword.getY()][maze.sword.getX()] = maze.sword.getState();
-
+		maze.mazeMap[maze.sword.getY()][maze.sword.getX()] = maze.sword
+				.getState();
 
 		maze.exit = new GameObject('S', maze.mazeDim - 1, maze.mazeDim - 5);
 		maze.mazeMap[maze.exit.getY()][maze.exit.getX()] = maze.exit.getState();
-		
+
 		// Dragons --->
-		maze.dragons=new ArrayList<DragonObject>();
+		maze.dragons = new ArrayList<DragonObject>();
 		maze.dragons.add(new DragonObject('D', 1, 3));
-		maze.mazeMap[maze.dragons.get(0).getY()][maze.dragons.get(0).getX()] = maze.dragons.get(0).getState();
-		if(d==2) DragonObject.enableCanMove();
-		if(d==3) {
+		maze.mazeMap[maze.dragons.get(0).getY()][maze.dragons.get(0).getX()] = maze.dragons
+				.get(0).getState();
+		if (d == 2)
+			DragonObject.enableCanMove();
+		if (d == 3) {
 			DragonObject.enableCanMove();
 			DragonObject.enableCanSleep();
 		}
-		if(nDragons>1){
-			for(int i=1; i<nDragons; i++){
-				maze.dragons.add(new DragonObject('D', 1+3+i, 3+3+i));
-				maze.mazeMap[maze.dragons.get(i).getY()][maze.dragons.get(i).getX()] = maze.dragons.get(i).getState();
+		if (nDragons > 1) {
+			for (int i = 1; i < nDragons; i++) {
+				maze.dragons.add(new DragonObject('D', 1 + 3 + i, 3 + 3 + i));
+				maze.mazeMap[maze.dragons.get(i).getY()][maze.dragons.get(i)
+						.getX()] = maze.dragons.get(i).getState();
 			}
 		}
 		// -->
-		
+
 	}
 
 	// this function evaluates if the game is over by checking if the hero is
 	// adjacent to the dragon and unarmed or if he's armed and exited the
 	// dungeon
 	public static boolean gameOver() {
-		if (GameObject.samePosition(maze.hero, maze.exit) && maze.hero.getState() == 'A')  return true;
-		for(int i=0; i<maze.dragons.size(); i++)
-			if (GameObject.adjacentPosition(maze.hero, maze.dragons.get(i)) && (maze.hero.getState() == 'H' && (maze.dragons.get(i).getState()=='D' || maze.dragons.get(i).getState()=='F')) ) return true;
-				
+		if (GameObject.samePosition(maze.hero, maze.exit)
+				&& maze.hero.getState() == 'A')
+			return true;
+		for (int i = 0; i < maze.dragons.size(); i++)
+			if (GameObject.adjacentPosition(maze.hero, maze.dragons.get(i))
+					&& (maze.hero.getState() == 'H' && (maze.dragons.get(i)
+							.getState() == 'D' || maze.dragons.get(i)
+							.getState() == 'F')))
+				return true;
+
 		return false;
 	}
 
