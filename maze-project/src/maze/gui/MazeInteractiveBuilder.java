d2ba8219278d5bb99dbc8460e3e14d7c787718ca
andrefreitas <p.andrefreitas@gmail.com>
2012-04-09 18:39:45 +0100

    Revert "Prevented placement of objects on the border of the maze"
    This reverts commit 599e95c3093086adfd936325de563564daa40d24.

--- maze-project/src/maze/gui/MazeInteractiveBuilder.java
+++ maze-project/src/maze/gui/MazeInteractiveBuilder.java
@@ -396,19 +396,18 @@ public class MazeInteractiveBuilder {
                     // Get the button position
                     int x = ((mazeCell) e.getSource()).x;
                     int y = ((mazeCell) e.getSource()).y;
-                    if (x != dimension - 1 && x != 0 && y != dimension - 1 && y != 0) {
-                        // Evaluate the ocurrence
-                        int oldVal = ((mazeCell) e.getSource()).val;
-                        int newVal = valueSelected;
-                        if (checkOcurrence(oldVal, newVal)) {
 
-                            // Set the icon and value in the button
-                            ((mazeCell) e.getSource()).setIcon(selectIcon(valueSelected));
-                            ((mazeCell) e.getSource()).setValue(valueSelected);
+                    // Evaluate the ocurrence
+                    int oldVal = ((mazeCell) e.getSource()).val;
+                    int newVal = valueSelected;
+                    if (checkOcurrence(oldVal, newVal)) {
 
-                            // Update the value in the maze array of chars
-                            maze[y][x] = valueSelected;
-                        }
+                        // Set the icon and value in the button
+                        ((mazeCell) e.getSource()).setIcon(selectIcon(valueSelected));
+                        ((mazeCell) e.getSource()).setValue(valueSelected);
+
+                        // Update the value in the maze array of chars
+                        maze[y][x] = valueSelected;
                     }
                 }
             });
@@ -500,13 +499,12 @@ public class MazeInteractiveBuilder {
 
         return true;
     }
-
     /**
-     * This function returns an ImageIcon depending on the value given. Example:
-     * if the val given is 'H', this returns an Hero icon.
-     *
+     * This function returns an ImageIcon depending on the value given.
+     * Example: if the val given is 'H', this returns an Hero icon.
      * @param val the character value of the icon
      */
+
     public ImageIcon selectIcon(char val) {
         ImageIcon aux;
         aux = (ImageIcon) sandIcon;
@@ -529,4 +527,5 @@ public class MazeInteractiveBuilder {
         }
         return aux;
     }
+
 }
